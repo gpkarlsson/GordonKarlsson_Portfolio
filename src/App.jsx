e80c3ab2bdf3d7f57426bdc48b353972d2471937
Gordon Karlsson <gpkarlsson72@gmail.com>
2023-05-17 15:36:50 -0500

    working on setting up components and router

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,35 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import {
+  BrowserRouter,
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Routes
+} from 'react-router-dom'
 
+import rootLoader from './root-loader'
+import aboutLoader from './about-loader'
+
+const router = createBrowserRouter(
+
+  
+)
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <BrowserRouter router={router}>
+    <Routes>
+      <Route path="/" element={<rootLoader />}>
+
+
+      </Route>
+    </Routes>
+    </BrowserRouter>
+      
     </>
   )
 }
