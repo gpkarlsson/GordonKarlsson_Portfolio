d8bb4180f0c56050311040a3c51ec30ab1a63081
Gordon Karlsson <gpkarlsson72@gmail.com>
2023-06-22 14:49:35 -0500

    working on navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,27 @@
+import { Stack } from '@chakra-ui/react'
 import React from 'react'
 import { Link } from 'react-router-dom'
 export default function Navbar() {
   return (
     <>
-    <div>Navbar</div>
+    <Stack direction="row" spacing={4} align="center">
+      <Flex
+        py={2}
+        as={Link}
+        href={href ?? '#'}
+        justify={'space-between'}
+        align={'center'}
+        _hover={{
+        textDecoration: 'none',
+        }}>
     <ul>
       <li><Link to="/dashboard">Home</Link></li>
       <li><Link to="/about">About</Link></li>
       <li><Link to="/projects">Projects</Link></li>
       <li><Link to="/resume">Resume</Link></li>
     </ul>
+    </Flex>
+    </Stack>
     </>
   )
 }
